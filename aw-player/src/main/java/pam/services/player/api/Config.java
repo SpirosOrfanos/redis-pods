ff0754ec50c542a69408b4610b8f8ba8d01bc580
sorfanos <s.orfanos@opap.gr>
2026-04-26 19:39:22 +0300

    clean up logs

--- aw-player/src/main/java/pam/services/player/api/Config.java
+++ aw-player/src/main/java/pam/services/player/api/Config.java
@@ -3,6 +3,8 @@ package pam.services.player.api;
 
 import aw.libs.redislib.configuration.InvalidationMessage;
 import aw.libs.redislib.configuration.MultiTierCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -17,11 +19,14 @@ public class Config {
     @Autowired
     private CacheManager cacheManager;
 
+    private static final Logger logger = LoggerFactory.getLogger(MultiTierCacheManager.class);
+
+
     @Autowired
     private ApplicationEventPublisher eventPublisher;
 
-    @GetMapping
-    public String cll(@RequestParam(name = "type", defaultValue = "none") String type) {
+    @GetMapping(path = "/owner")
+    public String owner(@RequestParam(name = "type", defaultValue = "none") String type) {
 
         Cache cache = cacheManager.getCache("ownerCache");
 
@@ -30,29 +35,50 @@ public class Config {
             var response = multiTierCache.get(type);
             if (response == null) {
                 return multiTierCache.get(type, () -> {
-                    var event = new InvalidationMessage(InvalidationMessage.Type.EVICT, "ownerCache", type);
-
-                    eventPublisher.publishEvent(event);
-                    System.out.println("from cache");
+                    logger.debug("from cache");
                     return "ownerRepository.findByType" + (type);
                 });
             }
-            System.out.println("direct return");
+            logger.debug("direct return");
             return response.toString();
 
         }
         var event = new InvalidationMessage(InvalidationMessage.Type.EVICT, "ownerCache", type);
         eventPublisher.publishEvent(event);
-        System.out.println("publish from db");
+        logger.debug("publish from db");
         return "ownerRepository.findByType" + (type);
+    }
+
+    @GetMapping(path = "/player")
+    public String player(@RequestParam(name = "type", defaultValue = "none") String type) {
+
+        Cache cache = cacheManager.getCache("playerCache");
+
+        if (cache instanceof MultiTierCacheManager.MultiTierCache) {
+            MultiTierCacheManager.MultiTierCache multiTierCache = (MultiTierCacheManager.MultiTierCache) cache.getNativeCache();
+            var response = multiTierCache.get(type);
+            if (response == null) {
+                return multiTierCache.get(type, () -> {
+                    logger.debug("player from cache");
+                    return "playerRepository.findByType" + (type);
+                });
+            }
+            logger.debug("direct return player");
+            return response.toString();
+
+        }
+        var event = new InvalidationMessage(InvalidationMessage.Type.EVICT, "playerCache", type);
+        eventPublisher.publishEvent(event);
+        logger.debug("player publish from db");
+        return "playerRepository.findByType" + (type);
     }
 
 
     @EventListener
     public void handleOwnerUpdate(InvalidationMessage event) {
-        System.out.println("handleOwnerUpdate "+event);
-        Cache cache = cacheManager.getCache("ownerCache");
-        cache.evict(event.getType());
+        System.out.println("handleOwnerUpdate "+event.getCacheName());
+        cacheManager.getCache(event.getCacheName()).evict(event.getType());
+        cacheManager.getCache(event.getCacheName()).evict(event.getType());
     }
 
 }
